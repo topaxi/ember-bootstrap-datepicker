51f392e1c6fb211f9308027fae714a2bc8faca9a
Damian Senn <senn@puzzle.ch>
2017-10-26 08:50:23 +0200

    Update ember-cli

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'dummy',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -43,7 +44,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    // here you can enable a production-specific feature
   }
 
   if (environment === 'production') {
